8ad94e8bbc0adbaba640ecdc2ed0d3f65faf0573
Faqih Ardiansyah <mofaqihardiansyah@gmail.com>
2025-11-03 07:51:49 +0700

    update: menambahkan route /pasien/me

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PasienController;
 
 // Route bebas (guest)
 Route::get('/check', fn() => response()->json(['message' => 'API aktif']));
@@ -11,5 +12,7 @@ Route::post('/login', [AuthController::class, 'login']);
 // ✅ Route yang butuh token
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    // Route ini untuk mengambil data pasien
+    Route::get('/pasien/me', [PasienController::class, 'me']);
     // Tambahkan route lain yang harus login di sini
 });
