6eab5254cf24fd3abe852e453cb89bf7db368d46
Wais Alqurni <waisofficial60@gmail.com>
2025-11-20 13:55:52 +0700

    Update routes dan tambah folder auth web

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// 1. Halaman Utama (Wajib GET agar bisa dibuka di browser)
 Route::get('/', function () {
-    return view('dashboard');
+    return view('auth/login'); // Pastikan file resources/views/welcome.blade.php ada
 });
-Route::get('/dashboard
-', function () {
-    return view('dashboard');
-});
+
+// 2. Proses Login (Wajib POST untuk menerima data form)
+Route::post('/login', [AuthController::class, 'login'])->name('login');
