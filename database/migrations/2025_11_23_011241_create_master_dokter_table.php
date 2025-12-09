f4bbe05f38b8bf0f4ac45510926407b897cf9756
Wais Alqurni <waisofficial60@gmail.com>
2025-12-09 08:50:11 +0700

    migration create_master_dokter_table

--- database/migrations/2025_11_23_011241_create_master_dokter_table.php
+++ database/migrations/2025_11_23_011241_create_master_dokter_table.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('master_dokter', function (Blueprint $table) {
@@ -17,11 +14,18 @@ return new class extends Migration
             $table->string('kode_dokter', 15);
             $table->string('nama', 50);
             $table->string('gelar', 50);
-            $table->string('spesialisasi', 50);
+
+            // sesuai screenshot: INT, bukan varchar
+            $table->integer('spesialisasi');
+
+            // sesuai screenshot: varchar(55)
+            $table->string('file_foto', 55);
+
             $table->string('alamat', 50);
             $table->string('hp', 15);
 
-            $table->integer('tipe')->default(1)->nullable();
+            // sesuai screenshot: int default 1, NOT NULL
+            $table->integer('tipe')->default(1);
 
             $table->string('dokter_str', 250);
             $table->date('dokter_str_mulai')->default('1960-01-01');
@@ -38,9 +42,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('master_dokter');
